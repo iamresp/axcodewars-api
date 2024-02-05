4fe4e3f217b4b715b1adfb5da4f8ccaf0dad8394
Evgeniy Grigorev <evg.v.grigorev@gmail.com>
2024-02-05 11:39:03 +0300

    fix: bootstrap uploads

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { WsAdapter } from '@nestjs/platform-ws';
+import { existsSync } from 'fs';
+import { mkdir } from 'fs/promises';
+import { join } from 'path';
 import { AppModule } from './app';
 import * as bodyParser from 'body-parser';
 import { GLOBAL_PREFIX } from './common';
+import { UPLOADS_DIR } from './file';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +16,11 @@ async function bootstrap() {
   app.use(bodyParser.json());
   app.setGlobalPrefix(GLOBAL_PREFIX);
   app.useWebSocketAdapter(new WsAdapter(app));
+
+  if (!existsSync(join(process.cwd(), UPLOADS_DIR))) {
+    await mkdir(join(process.cwd(), UPLOADS_DIR));
+  }
+
   await app.listen(process.env.APP_PORT);
 }
 
